69ebeba999db6fae3576531d003411ae812857f9
C H <charles.hewitt@pathwayanalytics.com>
2024-01-10 15:01:19 +0000

    wip: admin area migrate table

--- packages/frontend/src/routes/temp/+page.svelte
+++ packages/frontend/src/routes/temp/+page.svelte
@@ -1,15 +1,44 @@
 <script lang='ts'>
-    import { onMount } from 'svelte';
-    import { up } from '$lib/migrate/04_adminAreas';
-  
-    onMount(async () => {
-      console.log('loading...');
-      
+import { onMount } from 'svelte';
+import { up } from '$lib/migrate/04_adminAreas';
+import { 
+  Table, TableHead, TableHeadCell,
+  TableBody,  TableBodyRow, TableBodyCell,  
+  Checkbox, TableSearch 
+} from 'flowbite-svelte';
+
+
+let data: any = [];
+
+onMount(async () => {
+  console.log('loading...');
       try {
         await up();
         console.log('Function executed successfully');
       } catch (error) {
         console.error('Error executing function', error);
       }
-    });
+});
   </script>
+  <Table {data}>
+    <TableHead>
+      <TableHeadCell>LSOA</TableHeadCell>
+      <TableHeadCell>Code</TableHeadCell>
+      <TableHeadCell>Name</TableHeadCell>
+    </TableHead>
+    <TableBody>
+      {#each data as row}
+        <TableBodyRow>
+          <TableBodyCell>
+            {row.lsoa}  
+          </TableBodyCell>
+          <TableBodyCell>
+            {row.code}  
+          </TableBodyCell>
+          <TableBodyCell>
+            {row.name}  
+          </TableBodyCell>
+        </TableBodyRow>
+      {/each}
+    </TableBody>
+  </Table>
