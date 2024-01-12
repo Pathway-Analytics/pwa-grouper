dc81edbf2e4de559034981cf4a6f2d00c3e7c747
C H <charles.hewitt@pathwayanalytics.com>
2024-01-12 19:11:53 +0000

    fix: set session Domain to . env DOMAIN

--- packages/functions/src/auth.ts
+++ packages/functions/src/auth.ts
@@ -135,6 +135,7 @@ function getSessionCookies (userId: string):{
         },
         options: {
             // expiresIn: 60 * 60 * 24 * 1000, // 1 day
+            Domain: `.${Config.DOMAIN}`,
         },
         // redirect to a Frontend page where the session is set in localstorage via hooks.client.ts
         redirect: `${Config.SITE_URL}/session`
