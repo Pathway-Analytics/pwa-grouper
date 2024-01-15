5b61543960d80535284964f31a466f0df2bf5d09
C H <charles.hewitt@pathwayanalytics.com>
2024-01-15 17:05:41 +0000

    try logging refreshSession

--- packages/frontend/src/lib/refreshSession.ts
+++ packages/frontend/src/lib/refreshSession.ts
@@ -3,9 +3,10 @@ import { emptySession, type SessionType  } from "@pwa-grouper/core/types/session
 
 // return session data of SessionType
 export const refreshSession = async (token?: string): Promise<{message: string, session: SessionType}> => {
+  console.log('0. refreshSession() started...');
   try {
     // include credentials: 'include' to send cookies
-    console.log('refreshSession mode: ', env.PUBLIC_MODE);
+    console.log('1. refreshSession() mode: ', env.PUBLIC_MODE);
     if (env.PUBLIC_MODE === 'local'){
       const  authHeader = {Authorization: `Bearer ${token}`};
       const res = await fetch(`${env.PUBLIC_API_URL}/session`,{
@@ -14,17 +15,20 @@ export const refreshSession = async (token?: string): Promise<{message: string, 
         credentials: 'include',
       });
       const data: {message: string, session: SessionType} = await res.json();
+      console.log('2. refreshSession() res: ', res);
       if (res.ok) {
         return data;
       }
       return data;  
 
     } else {
+      console.log('4.refreshSession() non-local mode: ', env.PUBLIC_MODE);
       const res = await fetch(`${env.PUBLIC_API_URL}/session`,{
         method: 'GET',
         credentials: 'include',
       })
       const data: {message: string, session: SessionType} = await res.json();
+      console.log('5. refreshSession() res: ', res);
       if (res.ok) {
         return data;
       }
