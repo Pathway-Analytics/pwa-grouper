e00799de4c56f7d50fcf6ca9d4eb2e3b102d4f4b
C H <charles.hewitt@pathwayanalytics.com>
2024-01-18 00:25:09 +0000

    wip: admiin areas fetch - client side

--- packages/frontend/src/routes/(authenticated)/(admin)/admin/admin_areas/+page.svelte
+++ packages/frontend/src/routes/(authenticated)/(admin)/admin/admin_areas/+page.svelte
@@ -1,8 +1,12 @@
 <script lang='ts'>
+	import { page } from '$app/stores';
     import { onMount } from 'svelte';
 	import { Table, Button, TableBodyRow, TableHeadCell, TableHead, TableBodyCell, TableBody } from 'flowbite-svelte';
 	import { AdminAreaTypeType as AdminArea_Type, type AdminAreaType, type AdminAreaTypeType } from '@pwa-grouper/core/types/adminArea';
 
+    let query: string = '';
+    let p: number = 1;
+    let pageSize: number = 1000;
     let collection: string = 'E92';
     let within: string = 'E92000001';
     let adminAreas: AdminAreaType[] = [];
@@ -30,10 +34,11 @@
     }
 
     interface bindings {
-        code: typeValue;
         link: typeValue;
+        code: typeValue;
         name: typeValue;
         status: typeValue;
+        within: typeValue;
     }
 
     interface JsonResponseObject {
@@ -45,19 +50,14 @@
         };
     }
 
-    async function fetchEntityRecords(collection: string, within: string) {
-        
+    async function fetchEntityRecords(collection: string, within: string, pageNumber: number = 1) {
+        p = pageNumber;
         // convert the csv collections string to an array of strings
         collections = collection.split(',');
         
-        const query : string = getSPARQL(collections, within);
+        query = getSPARQL(collections, within, p, pageSize);
         try {
-            interface SPARQLResponse {
-                link: string;
-                code: string;
-                name: string;
-                status: string;
-            }
+
             const response = await fetch(`https://statistics.data.gov.uk/sparql.json?query=${encodeURIComponent(query)}`, {
                 headers: {
                     Accept: 'application/json'
@@ -76,9 +76,9 @@
                 return {
                     code: resultsRecord.code.value,
                     name: resultsRecord.name.value,
-                    parentCode: within,
+                    parentCode: resultsRecord.within.value,
                     lastChanged: new Date(),
-                    isActive: true,
+                    isActive: resultsRecord.status.value.toLowerCase() === 'live',
                     type: getAdminAreaType(resultsRecord.code.value.substring(0, 3)),
                 }
             });
@@ -92,25 +92,53 @@
     }
 
 
-    function getSPARQL(collections: string[], within: string) {
+    function getSPARQL(collections: string[], within: string, pageNumber: number = 1, pageSize?: number) {
     
         const collectionString = collections.map(entity => 
             `<http://statistics.data.gov.uk/id/statistical-entity/${entity}>`).join('\n');
         
-        const query = `SELECT ?link ?code ?name ?status 
+        const paginationString = pageSize ? `LIMIT ${pageSize} OFFSET ${(pageNumber - 1) * pageSize}` : '';
+        const withinString = within ? `VALUES ?withinLink { <http://statistics.data.gov.uk/id/statistical-geography/${within}> }` : '';
+
+        const query = `SELECT ?link ?code ?name ?status ?within
             WHERE { 
-                VALUES ?entityCode {
-                    ${collectionString}
-                } 
+                VALUES ?entityCode {${collectionString}}
+                ${withinString}
             ?link <http://statistics.data.gov.uk/def/statistical-entity#code> ?entityCode ;
-                <http://www.w3.org/2004/02/skos/core#notation> ?code .
+            <http://www.w3.org/2004/02/skos/core#notation> ?code .
             ?link <http://publishmydata.com/def/ontology/foi/displayName>  ?name .
-            ?link <http://statistics.data.gov.uk/def/statistical-geography#status> ?status
-            FILTER (?status = "live")
+            ?link <http://statistics.data.gov.uk/def/statistical-geography#status> ?status .
+            ?link <http://publishmydata.com/def/ontology/foi/within> ?withinLink .
+            ?withinLink <http://www.w3.org/2004/02/skos/core#notation> ?within
             }
-            ORDER BY ASC(?code)`
+            ORDER BY ASC(?code)
+            ${paginationString}
+            `
+
+            
         return query;
     }
+
+// SELECT ?link ?code ?name ?status ?within
+// WHERE { 
+  
+//   #VALUES ?item {'?'} #'E01034292'}
+//    VALUES ?within {'E07000213'}
+//     VALUES ?entityCode { <http://statistics.data.gov.uk/id/statistical-entity/E01> }
+//     #VALUES ?withinLink { <http://statistics.data.gov.uk/id/statistical-geography/E07000213> }
+//     ?link <http://statistics.data.gov.uk/def/statistical-entity#code> ?entityCode ;
+//          <http://www.w3.org/2004/02/skos/core#notation> ?item .
+//     ?link <http://publishmydata.com/def/ontology/foi/displayName>  ?name .
+//     ?link <http://statistics.data.gov.uk/def/statistical-geography#status> ?status .
+//     ?link <http://publishmydata.com/def/ontology/foi/within> ?withinLink .
+//   ?withinLink <http://www.w3.org/2004/02/skos/core#notation> ?within
+
+//     FILTER (?status = "live")
+// }
+// ORDER BY ASC(?code)
+// LIMIT 10 
+// #?pageSize 
+// #OFFSET =(?pageNumber - 1) * ?pageSize
 
     function getAdminAreaType(code: string): AdminAreaTypeType {
 
@@ -145,12 +173,25 @@
 <!-- add two input fields for collections and within -->
 <input bind:value={collection} />
 <input bind:value={within} />
+<!-- add pagination buttons and dropdown for pagesize -->
+<Button on:click={() => fetchEntityRecords(collection, within, 1)}>First</Button>
+<Button on:click={() => fetchEntityRecords(collection, within, p-1)}>Previous</Button>
+<Button on:click={() => fetchEntityRecords(collection, within, p+1)}>Next</Button>
+<select bind:value={pageSize}>
+    <option value='10'>10</option>
+    <option value='100'>100</option>
+    <option value='1000'>1000</option>
+</select>
+<!-- display the number of records -->
+<p>Number of records: {adminAreas.length}</p>
 <div>
 <Table>
     <TableHead>
         <TableHeadCell>Code</TableHeadCell>
         <TableHeadCell>Name</TableHeadCell>
         <TableHeadCell>Type</TableHeadCell>
+        <TableHeadCell>Status</TableHeadCell>
+        <TableHeadCell>Within</TableHeadCell>
     </TableHead>
 
     <TableBody>
@@ -160,15 +201,18 @@
                     <TableBodyCell>{adminArea.code}</TableBodyCell>
                     <TableBodyCell>{adminArea.name}</TableBodyCell>
                     <TableBodyCell>{adminArea.type}</TableBodyCell>
+                    <TableBodyCell>{adminArea.isActive}</TableBodyCell>
+                    <TableBodyCell>{adminArea.parentCode}</TableBodyCell>
                 </TableBodyRow>
             {/each}
        {:else}
             <TableBodyRow >
-                <TableBodyCell colspan='3'>No admin areas found</TableBodyCell>
+                <TableBodyCell colspan='5'>No admin areas found</TableBodyCell>
             </TableBodyRow>
         {/if}
     </TableBody>
 </Table>
+{query}
 </div>
 
 <style>
